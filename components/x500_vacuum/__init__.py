ed5f4a4c69ceaec44413828813dcfef5a44192ac
johkn <107861937+johkn@users.noreply.github.com>
2025-06-11 15:31:36 +0200

    Update __init__.py

--- components/x500_vacuum/__init__.py
+++ components/x500_vacuum/__init__.py
@@ -1,5 +1,6 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
+from esphome.components import uart
 
 DEPENDENCIES = ["uart"]
 
@@ -15,7 +16,7 @@ X500VacuumComponent = x500_vacuum_ns.class_("X500VacuumComponent", cg.Component)
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(X500VacuumComponent),
-        cv.Required(CONF_UART_ID): cv.use_id(cg.UARTComponent),
+        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
         cv.Optional(CONF_RAWSTATE_ID): cv.string,
         cv.Optional(CONF_VACSTATE_ID): cv.string,
         cv.Optional(CONF_ERRORSTATE_ID): cv.string,
@@ -26,8 +27,8 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[cv.GenerateID()])
     await cg.register_component(var, config)
-    uart = await cg.get_variable(config[CONF_UART_ID])
-    cg.add(var.set_uart(uart))
+    uart_var = await cg.get_variable(config[CONF_UART_ID])
+    cg.add(var.set_uart(uart_var))
     if CONF_RAWSTATE_ID in config:
         cg.add(var.set_rawstate_id(config[CONF_RAWSTATE_ID]))
     if CONF_VACSTATE_ID in config:
