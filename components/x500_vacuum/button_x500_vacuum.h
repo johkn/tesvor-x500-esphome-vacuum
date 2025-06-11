56b07c30ddad668a71233ed48e4395df9752eb80
johkn <107861937+johkn@users.noreply.github.com>
2025-06-11 15:28:56 +0200

    Update button_x500_vacuum.h

--- components/x500_vacuum/button_x500_vacuum.h
+++ components/x500_vacuum/button_x500_vacuum.h
@@ -4,16 +4,22 @@
 namespace esphome {
 namespace x500_vacuum {
 
-static const char *const TAG = "x500_vacuum";
+class X500VacuumComponent : public Component {
+ public:
+  void setup() override {}
+  void loop() override {}
+  // ...
+};
 
-// Hier das YAML-Schema:
-static const auto CONFIG_SCHEMA = cv::Schema({
-  cv::Required("uart_id"): cv::string,
-  cv::Optional("rawstate_id"): cv::string,
-  cv::Optional("vacstate_id"): cv::string,
-  cv::Optional("errorstate_id"): cv::string,
-  cv::Optional("batdata_id"): cv::string,
-});
-...
-}
-}
+}  // namespace x500_vacuum
+}  // namespace esphome
+
+// Hier das YAML-Schema (meist in register_component() oder über esphome::register_component):
+ESPHOME_NAMESPACE_BEGIN
+ESPHOME_COMPONENT_SCHEMA(x500_vacuum::X500VacuumComponent)
+  .add_required("uart_id", esphome::cv::string)
+  .add_optional("rawstate_id", esphome::cv::string)
+  .add_optional("vacstate_id", esphome::cv::string)
+  .add_optional("errorstate_id", esphome::cv::string)
+  .add_optional("batdata_id", esphome::cv::string);
+ESPHOME_NAMESPACE_END
