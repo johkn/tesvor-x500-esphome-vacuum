4623f75cf4cebb1601a4014db9e57f4affaeb1d3
johkn <107861937+johkn@users.noreply.github.com>
2025-06-11 09:31:17 +0200

    Add files via upload

---
+++ x500_vacuum/button_x500_vacuum.cpp
@@ -0,0 +1,9 @@
+#include "button_x500_vacuum.h"
+
+namespace esphome {
+namespace x500_vacuum {
+
+// Nothing to implement, logic is inline in header.
+
+}  // namespace x500_vacuum
+}  // namespace esphome
